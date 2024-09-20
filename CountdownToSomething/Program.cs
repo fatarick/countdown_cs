027a62947ab4edecb114c4b08019f00f3e089721
Artem Litvintsev <61841951+fatarick@users.noreply.github.com>
2024-09-20 22:05:53 +0300

    Add files via upload

---
+++ CountdownToSomething/Program.cs
@@ -0,0 +1,14 @@
+﻿DateTime dateTime = DateTime.Now;
+Console.WriteLine("Welcome to the Countdowner! Developer is a normal man so write the date as a DD.MM.YYYY");
+Console.WriteLine("Write the date that you are waiting for: ");
+string date = Console.ReadLine();
+DateTime dateToCountdown = DateTime.ParseExact(date, "dd.MM.yyyy", null);
+
+while (true)
+{
+    dateTime = DateTime.Now; // Reuse the existing 'dateTime' variable
+    TimeSpan timeLeft = dateToCountdown - dateTime;
+    Console.WriteLine($"Time left: {timeLeft.Days} days, {timeLeft.Hours} hours, {timeLeft.Minutes} minutes, {timeLeft.Seconds} seconds");
+    System.Threading.Thread.Sleep(1000);
+    Console.Clear();
+}
